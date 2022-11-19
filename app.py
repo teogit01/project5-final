5de0ad6a5e93781c038ed2290474528b768c065a
teogit01 <teob1605272@gmail.com>
2022-11-19 15:36:43 +0700

    init some folders and files

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "<h1 style='text-align: center; font-size: 50px;'>Hello, This is EMPL1's Cluster!</h1>"
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
