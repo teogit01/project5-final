4c42626a821c4420615bdcd4b9b7d38db778e144
teogit01 <teob1605272@gmail.com>
2022-11-24 09:59:12 +0700

    fix lint

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 def home():
     return "<h1 style='text-align: center; font-size: 50px;'>Hello, This is EMPL1's Cluster!</h1>"
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
+    app.run(host='0.0.0.0', port=80, debug=True)
